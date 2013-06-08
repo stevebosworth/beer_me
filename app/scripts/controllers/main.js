8f55a83b8609420060aba9ba18cf413553b281ec
Lucas L <lucas@nuxbox.me>
2013-06-08 19:16:33 -0400

    updates
    Signed-off-by: Lucas L <lucas@nuxbox.me>

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,19 +1,17 @@
 'use strict';
+var json_data;
+function setJSON(data) {
+  json_data = data;
+}
 
 beerMeApp.controller('MainCtrl', function($scope, $http) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
 
-  var url = 'http://lcboapi.com/stores.json';
-    $http.get(url).success(function(data) {
-        console.log(data);
-        //$scope.results = data;
-    });
+  var url = 'http://lcboapi.com/stores?callback=setJSON';
+  $http.jsonp(url).success(function(data) {
+  }).error(function(data, status, headers, config) {
 
+    if (json_data.status == 200)
+      $scope.awesomeThings = json_data.result;
+  });
 
 });
-
-

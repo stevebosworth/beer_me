25e69b243f3bf5dfcb846feac816bd5120014ffe
Steve Bosworth <stevebosworth@gmail.com>
2013-06-08 15:14:03 -0400

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var beerMeApp = angular.module('beerMeApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+beerMeApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});

0a36f6e21f22236926c3683c36106e088310c48b
Neil Fenton <neilff@gmail.com>
2013-06-09 12:33:49 -0400

    Application is pulling data and supplies a details view for the selected store - Added bootstrap files manually, my SASS didn't like the compass files, will have to bring back later - JSONP issue can be resolved if the LCBO API creator unlocks periods (.) for CALLBACK function names, until then we are using the sloppy call back proceedure

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,91 @@
 'use strict';
 
-var beerMeApp = angular.module('beerMeApp', [])
-  .config(function($httpProvider){
-      delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  })
-  .config(['$routeProvider', function($routeProvider) {
+/* Routing */
+angular.module('beerMeApp', ['beerMeServices', 'beerMeFilters'])
+.config(function($httpProvider){
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+})
+.config(['$routeProvider', function($routeProvider) {
     $routeProvider
-      .when('/', {
+    .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
+        controller: 'listCtrl'
+    })
+    .when('/store/:storeId', {
+        templateUrl: 'views/details.html',
+        controller: 'detailsCtrl'
+    })    
+    .otherwise({
         redirectTo: '/'
-      });
-  }]);
+    });
+}]);
+
+/* Services */
+angular.module('beerMeServices', ['ngResource'])
+.factory('Store', function($http) {
+    return {
+        // returns data for closest 25 stores
+        getStoresList: function(position) {
+            return $http({
+                url: 'http://lcboapi.com/stores',
+                method: 'JSONP',
+                params: {
+                    callback: 'setJSON',
+                    per_page: '25',
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude
+                }
+            })
+        },
+        getStore: function(id) {
+            return $http({
+                url: 'http://lcboapi.com/stores/' + id,
+                method: 'JSONP',
+                params: {
+                    callback: 'setJSON'
+                }
+            })
+        }
+    }
+})
+
+// Geolocation factory
+// Adapted from Brian Ford: http://briantford.com/blog/angular-phonegap.html
+.factory('geoLocation', function ($rootScope) {
+  return {
+    getCurrentPosition: function (onSuccess, onError, options) {
+      navigator.geolocation.getCurrentPosition(function () {
+        var that = this,
+          args = arguments;
+
+        if (onSuccess) {
+          $rootScope.$apply(function () {
+            onSuccess.apply(that, args);
+          });
+        }
+      }, function () {
+        var that = this,
+          args = arguments;
+
+        if (onError) {
+          $rootScope.$apply(function () {
+            onError.apply(that, args);
+          });
+        }
+      },
+      options);
+    }
+  };
+});
+
+angular.module('beerMeFilters', [])
+.filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+})
+.filter('yesno', function() {
+    return function(input) {
+        return input ? 'Yes' : 'No';
+    };
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,67 @@
 'use strict';
+
+// --------------------------------------------------------------------
+/**
+ * JSONP Callback Fix
+ *
+ * Fix for LCBO api issue: doesn't support Angular style callback functions
+ * the API wants a function with only numbers, underscores, and letters
+ * Angular uses a function title: angular.callbacks._xxx
+ * which throws an error when used with the LCBO API
+ *
+ */
+
 var json_data;
 function setJSON(data) {
-  json_data = data;
+	json_data = "";
+	json_data = data;
 }
 
-beerMeApp.controller('MainCtrl', function($scope, $http) {
+// --------------------------------------------------------------------
+/**
+ * listCtrl
+ *
+ * Used to show the list of all the stores available
+ *
+ */
 
-  var url = 'http://lcboapi.com/stores?callback=setJSON';
-  $http.jsonp(url).success(function(data) {
-  }).error(function(data, status, headers, config) {
+function listCtrl($scope, $rootScope, Store, geoLocation) {
 
-    if (json_data.status == 200)
-      $scope.awesomeThings = json_data.result;
-  });
+	$scope.storesList = [];
 
-});
+	// get users current location
+	geoLocation.getCurrentPosition(function (position) {
+
+		// pass the users position to the getStoresList function
+		Store.getStoresList(position).success(function(data) {
+			$scope.storesList = data.result;
+		}).error(function(data, status) {
+			if (json_data.status == 200)
+				$scope.storesList = json_data.result;
+		});
+
+		// show users current location
+		$scope.currentLocation = position.coords.latitude + ',' + position.coords.longitude;
+	}, function() { alert('Failed to connect to GeoLocation'); });
+};
+
+// --------------------------------------------------------------------
+/**
+ * detailsCtrl
+ *
+ * Used to display data of the selected store 
+ *
+ */
+
+function detailsCtrl($scope, $rootScope, $routeParams, Store) {
+	$scope.storeId = $routeParams.storeId;
+
+	// retrieve the data for the selected store
+	Store.getStore($routeParams.storeId).success(function(data) {
+		$scope.store = data.result;
+	}).error(function(data, status) {
+		if (json_data.status == 200)
+			$scope.store = json_data.result;
+	});
+
+}

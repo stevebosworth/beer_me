56215620fdfb05401502a26f90ee84e97b632639
Steve Bosworth <stevebosworth@gmail.com>
2013-06-10 23:59:18 -0400

    Updated product details page to include 20 nearest locations with stock

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -24,7 +24,7 @@ function setJSON(data) {
  *
  */
 
-function listCtrl($scope, $rootScope, Store, geoLocation, $log) {
+function listCtrl($scope, $rootScope, $filter, Store, geoLocation, $log) {
 
     // Enable the new Google Maps visuals until it gets enabled by default.
     // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
@@ -39,8 +39,6 @@ function listCtrl($scope, $rootScope, Store, geoLocation, $log) {
 	// default zoom
 	$scope.zoom = 12;
 
-	// ******** testing ********
-	// from: http://nlaplante.github.io/angular-google-maps/#!/api
 	// $scope.eventsProperty = {
 	// 	click: function (mapModel, eventName, originalEventArgs) {
 	// 		// 'this' is the directive's scope
@@ -60,7 +58,7 @@ function listCtrl($scope, $rootScope, Store, geoLocation, $log) {
 			$scope.currentLocation = { latitude: position.coords.latitude, longitude: position.coords.longitude };
 			// show users current location on map
 			$scope.center = { latitude: position.coords.latitude, longitude: position.coords.longitude };
-			$scope.markers = [{ latitude: position.coords.latitude, longitude: position.coords.longitude }];
+			$scope.markers = [];
 
 			// pass the users position to the getStoresList function
 			Store.getStoresList(position, $scope.radius).success(function(data) {
@@ -71,16 +69,30 @@ function listCtrl($scope, $rootScope, Store, geoLocation, $log) {
 
 					// draw the markers onto the map
 					angular.forEach(json_data.result, function(object) {
-						$scope.markers.push({ latitude: object.latitude, longitude: object.longitude });
+						var html = '<div style="min-width: 300px; height: 150px;"">'
+<<<<<<< HEAD
+									+ '<p class="lead">' + object.name + '<br /><small>Store ID: ' + object.id + ''
+									+ '<br />' + object.address_line_1 + ' ' + ((object.address_line_2 != null) ? object.address_line_2 : "")
+=======
+									+ '<p class="lead">' + object.name + '<br /><small>Store ID: ' + object.id + '' 
+									+ '<br />' + object.address_line_1 + ' ' + ((object.address_line_2 != null) ? object.address_line_2 : "")  
+>>>>>>> ef23132b045b8e65fa15698aac273c8494288d58
+									+ '<br />' + $filter('is_open')(object) + '</small></p></div>';
+						$scope.markers.push({ latitude: object.latitude, longitude: object.longitude, infoWindow: html });
 					})
+
+<<<<<<< HEAD
+=======
+					//console.log($scope.markers);
 						
+>>>>>>> ef23132b045b8e65fa15698aac273c8494288d58
 				}
 			});
 
 		}, function() { alert('Failed to connect to GeoLocation'); });
 	}
 
-	// for some reason this fixed my query issue: filtered.length was showing 
+	// for some reason this fixed my query issue: filtered.length was showing
 	// incorrectly in the view, which the next function is reliant on, doesn't make sense
 	// will investigate later
     $scope.$watch('query', function (newValue) {
@@ -142,19 +154,54 @@ function productsListCtrl($scope, Products) {
  * productDetailsCtrl
  *
  * Used to display detailed view of a product
+ * Includes Stock Information and returns 20 closest stores
  *
  */
 
-function productDetailsCtrl($scope, $routeParams, Products) {
+function productDetailsCtrl($scope, $routeParams, geoLocation, Products) {
     //$scope.productId = $routeParams.productId;
+    geoLocation.getCurrentPosition(function (position) {
 
-    // retrieve the data for the selected store
-    Products.getProduct($routeParams.productId).success(function(data) {
-        $scope.product = data.result;
-    }).error(function(data, status) {
-        if (json_data.status == 200){
-            $scope.product = json_data.result;
-            //console.log($scope.product);
-        }
+        $scope.currentLocation = {latitude: position.coords.latitude, longitude: position.coords.longitude};
+        // show users current location on map
+
+        // retrieve the data for the selected store
+        Products.getProduct($routeParams.productId, position, 1).success(function(data) {
+            $scope.product = data.product;
+            $scope.store = data.result;
+        }).error(function(data, status) {
+            if (json_data.status == 200){
+                //set default images
+                if(!json_data.product['image_thumb_url']){
+                    json_data.product['image_thumb_url'] = "img/glyphicons-halflings.png";
+                }
+                $scope.product = json_data.product;
+                $scope.storesList = json_data.result;
+                //console.log($scope.product);
+            }
+        });
     });
+}
+
+// --------------------------------------------------------------------
+/**
+ * Products in Store by Query listCtrl
+ *
+ * Used to display a list of products based on a query.
+ *
+ */
+
+function storesForProductCtrl($scope, $routeParams, Products) {
+
+
+        Products.getStoresForProduct($routeParams.productId).success(function(data){
+            $scope.storesList = data.result;
+            console.log($scope.storesList);
+        }).error(function(data, status) {
+            if (json_data.status == 200){
+                $scope.storesList = json_data.result;
+                console.log($scope.storesList);
+            }
+        });
+        //$scope.productSearch();
 }
